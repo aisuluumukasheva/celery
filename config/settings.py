032989d6cfc5cbe49081dcfe9cb69523c79dc182
aisuluumukasheva <is6116@gmail.com>
2023-01-09 10:51:55 +0600

    changed ip

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['35.184.123.247', '127.0.0.1']
+ALLOWED_HOSTS = ['34.88.74.59', '127.0.0.1']
 
 
 # Application definition
